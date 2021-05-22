83769df6c29977bbb7621136a8b3729e542da5ae
Angela Man <angela.k.man@gmail.com>
2021-05-22 00:01:36 -0500

    create server and routes to html and notes data

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,39 @@
+const router = require("express").Router();
+const { notes } = require("../../db/db");
+const { createNewNote, validateNote, getNoteById } = require("../../lib/notes");
+const generateUniqueId = require("generate-unique-id");
+
+router.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+router.get('/notes/:id', (req, res) => {
+    const result = getNoteById(req.params.id, notes);
+    
+    if (result) {
+        res.json(result);
+    }
+    else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    // set unique id to each note
+    req.body.id = generateUniqueId({
+        length: 3,
+        useLetters: false
+    });
+    console.log(req.body);
+
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+        res.status(400).send("The note is missing a title or message.")
+    }
+    else {
+        const note = createNewNote(req.body, notes);
+        res.json(note);
+    }
+});
+
+module.exports = router;
